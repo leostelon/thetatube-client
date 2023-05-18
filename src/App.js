6bf729f3ccf816bc22be69135828a2b32932a8a1
oBLAZERo2001 <hari9112001@gmail.com>
2023-05-18 20:00:48 +0530

    navbar set up

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./screens/Home";
 import { Upload } from "./screens/Upload";
+import Profile from "./screens/Profile";
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 			<Routes>
 				<Route path="/" exact element={<Home />} />
 				<Route path="/upload" exact element={<Upload />} />
+				<Route path="/profile" exact element={<Profile />} />
 			</Routes>
 		</Router>
 	);
