9374b26a199f774d5bddc1704d53a15ab8326cf0
oBLAZERo2001 <hari9112001@gmail.com>
2023-05-19 00:57:46 +0530

    home page video card design completed

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,4 +1,5 @@
 import { Box } from "@mui/material";
+import styled from "styled-components";
 import "../styles/Home.css";
 import React from "react";
 import { Navbar } from "../components/Navbar";
@@ -7,6 +8,103 @@ export const Home = () => {
 	return (
 		<Box>
 			<Navbar />
+			<VideoCardHolder>
+				{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(() => (
+					<VideoCard>
+						<CardThumNail>
+							<img
+								src="/images/hq720.webp"
+								height="100%"
+								width="100%"
+								alt=""
+								style={{ borderRadius: "10px" }}
+							/>
+						</CardThumNail>
+
+						<CardDetailsContainer>
+							<CardProfile>
+								<img
+									src="/images/profileImg.jpg"
+									height="100%"
+									width="100%"
+									alt=""
+									style={{ borderRadius: "50%" }}
+								/>
+							</CardProfile>
+							<CardDetails>
+								<CardTitle>I GOT MARRIED ❤️ - Irfan's View</CardTitle>
+								<CardSmall>Random Chikibum</CardSmall>
+								<CardSmall>
+									650K views
+									<CardSmallSpan>2 weeks ago</CardSmallSpan>
+								</CardSmall>
+							</CardDetails>
+						</CardDetailsContainer>
+					</VideoCard>
+				))}
+			</VideoCardHolder>
 		</Box>
 	);
 };
+
+const VideoCardHolder = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-evenly;
+`;
+
+const VideoCard = styled.div`
+	/* height: 360px; */
+	height: 320px;
+	width: 340px;
+	/* background-color: black; */
+
+	margin-right: 10px;
+
+	&:hover {
+		cursor: pointer;
+	}
+`;
+
+const CardThumNail = styled.div`
+	width: 100%;
+	height: 200px;
+`;
+
+const CardDetailsContainer = styled.div`
+	display: flex;
+	margin-top: 12px;
+`;
+
+const CardProfile = styled.div`
+	height: 36px;
+	width: 36px;
+	margin-right: 12px;
+`;
+
+const CardDetails = styled.div`
+	overflow-x: hidden;
+	padding-right: 24px;
+	max-width: 250px;
+`;
+
+const CardTitle = styled.div`
+	font-size: 16px;
+	font-weight: 500;
+	margin-bottom: 6px;
+
+	color: #f1f1f1;
+`;
+
+const CardSmall = styled.div`
+	font-size: 12px;
+	font-weight: 400;
+
+	color: #aaa;
+`;
+const CardSmallSpan = styled.span`
+	:before {
+		content: "•";
+		margin: 0 4px;
+	}
+`;
--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -4,95 +4,95 @@ import { Avatar, Box, Button, Divider, Grid } from "@mui/material";
 import { deepOrange } from "@mui/material/colors";
 
 export default function Profile() {
-  return (
-    <>
-      <Navbar />
-      <Box sx={{ pl: 4 }}>
-        <Grid container sx={{ color: "white" }}>
-          <Grid iteam>
-            <Avatar
-              alt="Remy Sharp"
-              src="/broken-image.jpg"
-              sx={{
-                bgcolor: "#f45966",
-                height: "100px",
-                width: "100px",
-                mr: 3,
-              }}
-            >
-              B
-            </Avatar>
-          </Grid>
-          <Grid
-            iteam
-            sx={{ pb: 2 }}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              flexDirection: "column",
-            }}
-          >
-            <Box sx={{ fontSize: 24 }}>hari prasana</Box>
-            <Box sx={{ mt: 1, fontSize: 14 }}>
-              @hariprasana1234r5 No subscribers No videos
-            </Box>
-          </Grid>
-        </Grid>
-        {/* secondry nav */}
-        <>
-          <Grid container spacing={2} sx={{ mt: 3, ml: 2 }}>
-            <Grid iteam xs={2} md={1} sx={{}}>
-              <Button sx={{ color: "gray" }}>Home</Button>
-            </Grid>
-            {/* <Grid iteam xs={2} md={1} sx={{}}>
+	return (
+		<>
+			<Navbar />
+			<Box sx={{ pl: 4 }}>
+				<Grid container sx={{ color: "white" }}>
+					<Grid iteam>
+						<Avatar
+							alt="Remy Sharp"
+							src="/broken-image.jpg"
+							sx={{
+								bgcolor: "#f45966",
+								height: "100px",
+								width: "100px",
+								mr: 3,
+							}}
+						>
+							B
+						</Avatar>
+					</Grid>
+					<Grid
+						iteam
+						sx={{ pb: 2 }}
+						style={{
+							display: "flex",
+							justifyContent: "center",
+							flexDirection: "column",
+						}}
+					>
+						<Box sx={{ fontSize: 24 }}>hari prasana</Box>
+						<Box sx={{ mt: 1, fontSize: 14 }}>
+							@hariprasana1234r5 No subscribers No videos
+						</Box>
+					</Grid>
+				</Grid>
+				{/* secondry nav */}
+				<>
+					<Grid container spacing={2} sx={{ mt: 3, ml: 2 }}>
+						<Grid iteam xs={2} md={1} sx={{}}>
+							<Button sx={{ color: "gray" }}>Home</Button>
+						</Grid>
+						{/* <Grid iteam xs={2} md={1} sx={{}}>
               <Button>Home</Button>
             </Grid> */}
-          </Grid>
-        </>
-        <hr style={{ backgroundColor: "gray" }} />
-      </Box>
+					</Grid>
+				</>
+				<hr style={{ backgroundColor: "gray" }} />
+			</Box>
 
-      <Box
-        style={{
-          width: "100%",
-          display: "flex",
-          //   justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-        }}
-      >
-        <Box
-          style={{
-            width: "200px",
-            height: "200px",
-          }}
-        >
-          <img
-            style={{ width: "100%", height: "100%" }}
-            src="/images/profile_video_notFound.svg"
-            alt=""
-            srcset=""
-          />
-        </Box>
+			<Box
+				style={{
+					width: "100%",
+					display: "flex",
+					//   justifyContent: "center",
+					alignItems: "center",
+					flexDirection: "column",
+				}}
+			>
+				<Box
+					style={{
+						width: "200px",
+						height: "200px",
+					}}
+				>
+					<img
+						style={{ width: "100%", height: "100%" }}
+						src="/images/profile_video_notFound.svg"
+						alt=""
+						srcset=""
+					/>
+				</Box>
 
-        <Box sx={{ fontSize: 24 }}>Upload a video to get started</Box>
+				<Box sx={{ fontSize: 24 }}>Upload a video to get started</Box>
 
-        <Box
-          sx={{
-            fontSize: 14,
-            mt: 2,
-            maxWidth: "480px",
-            textAlign: "center",
-            color: "gray",
-          }}
-        >
-          Start sharing your story and connecting with viewers. Videos that you
-          upload will show up here.
-        </Box>
-        <Button variant="outlined" sx={{ br: 5, mt: 2 }}>
-          Upload Video
-        </Button>
-      </Box>
-    </>
-  );
+				<Box
+					sx={{
+						fontSize: 14,
+						mt: 2,
+						maxWidth: "480px",
+						textAlign: "center",
+						color: "gray",
+					}}
+				>
+					Start sharing your story and connecting with viewers. Videos that you
+					upload will show up here.
+				</Box>
+				<Button variant="outlined" sx={{ br: 5, mt: 2 }}>
+					Upload Video
+				</Button>
+			</Box>
+		</>
+	);
 }
