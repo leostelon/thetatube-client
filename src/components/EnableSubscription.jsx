601e2a7056fb8e1f1d7e4522a8c7edd20af83ac8
oBLAZERo2001 <hari9112001@gmail.com>
2023-05-20 00:15:19 +0530

    enable subscription inputs added

--- src/components/EnableSubscription.jsx
+++ src/components/EnableSubscription.jsx
@@ -2,12 +2,23 @@ import React, { useEffect, useState } from "react";
 import ThetaTubeFactoryInterface from "../contracts/ThetaTubeFactory.json";
 import { getWalletAddress, switchChain } from "../utils/wallet";
 import { CONTRACT_ADDRESS } from "../constants";
-import { Box, CircularProgress, Dialog } from "@mui/material";
+import {
+	Box,
+	Button,
+	CircularProgress,
+	Dialog,
+	FormControl,
+	TextField,
+} from "@mui/material";
 import { enableSubscription } from "../database/user";
 
 export const EnableSubscription = ({ isOpen, handleExternalClose }) => {
 	const [loading, setLoading] = useState(false);
 	const [open, setOpen] = useState(false);
+
+	const [tokenName, setTokenName] = useState();
+	const [tokenSymbol, setTokenSymbol] = useState();
+	const [tokenPrice, setTokenPrice] = useState();
 
 	async function createSubscription() {
 		setLoading(true);
@@ -21,13 +32,13 @@ export const EnableSubscription = ({ isOpen, handleExternalClose }) => {
 		// Gas Calculation
 		const gasPrice = await window.web3.eth.getGasPrice();
 		const gas = await contract.methods
-			.createToken("token name", "token symbol", 2, currentAddress) // change 2 to price taken from field
+			.createToken(tokenName, tokenSymbol, tokenPrice, currentAddress) // change 2 to price taken from field
 			.estimateGas({
 				from: currentAddress,
 			});
 
 		const resp = await contract.methods
-			.createToken("token name", "token symbol", 2, currentAddress) // change 2 to price taken from field
+			.createToken(tokenName, tokenSymbol, tokenPrice, currentAddress) // change 2 to price taken from field
 			.send({ from: currentAddress, gasPrice, gas })
 			.on("transactionHash", function (hash) {
 				// setStatus(3);
@@ -60,30 +71,74 @@ export const EnableSubscription = ({ isOpen, handleExternalClose }) => {
 			<Box
 				sx={{
 					p: 2,
-					textAlign: "center",
+					// textAlign: "center",
 					width: "100%",
-					backgroundColor: "#171e25",
+					backgroundColor: "#46505a",
 					color: "white",
 				}}
 			>
 				<Box>
 					<h2>Create Subscription NFT</h2>
 					<br />
-					<Box
-						sx={{
-							cursor: "pointer",
-							backgroundColor: "#256afe",
-							borderRadius: "4px",
-							p: 2,
-							minWidth: "100px",
-						}}
-						onClick={() => createSubscription()}
-					>
-						{loading ? (
-							<CircularProgress size={14} sx={{ color: "white" }} />
-						) : (
-							"Create Subscription NFT"
-						)}
+					<Box sx={{ bgColor: "white", width: "80%" }}>
+						<FormControl fullWidth>
+							<TextField
+								id="tokenName"
+								label="Token Name"
+								variant="outlined"
+								value={tokenName}
+								onChange={(e) => {
+									setTokenName(e.target.value);
+								}}
+								size="small"
+							/>
+						</FormControl>
+
+						<FormControl fullWidth sx={{ mt: 2 }}>
+							<TextField
+								id="tokenSymbol"
+								label="Token Symbol"
+								variant="outlined"
+								value={tokenSymbol}
+								onChange={(e) => {
+									setTokenSymbol(e.target.value);
+								}}
+								size="small"
+							/>
+						</FormControl>
+
+						<FormControl fullWidth sx={{ mt: 2 }}>
+							<TextField
+								id="tokenPrice"
+								label="Token Price"
+								variant="outlined"
+								value={tokenPrice}
+								onChange={(e) => {
+									setTokenPrice(e.target.value);
+								}}
+								size="small"
+							/>
+						</FormControl>
+					</Box>
+					<Box sx={{ textAlign: "right" }}>
+						<Button
+							sx={{
+								// cursor: "pointer",
+								// backgroundColor: "#256afe",
+								// borderRadius: "4px",
+								// p: 2,
+								// minWidth: "100px",
+								mt: 2,
+							}}
+							variant="contained"
+							onClick={() => createSubscription()}
+						>
+							{loading ? (
+								<CircularProgress size={14} sx={{ color: "white" }} />
+							) : (
+								"Create Subscription NFT"
+							)}
+						</Button>
 					</Box>
 				</Box>
 			</Box>
